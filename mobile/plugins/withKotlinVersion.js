e2c49eedcc5f998806f91b3745c8d1fa30d76159
CCDS Citoyen <ccds@guyane.fr>
2026-03-05 21:47:47 -0500

    fix(mobile): config plugin removes enableBundleCompression from build.gradle (RN 0.76 compat)

--- mobile/plugins/withKotlinVersion.js
+++ mobile/plugins/withKotlinVersion.js
@@ -1,12 +1,11 @@
-const { withGradleProperties } = require('expo/config-plugins');
+const { withGradleProperties, withAppBuildGradle } = require('expo/config-plugins');
 
-module.exports = function withKotlinVersion(config) {
-  return withGradleProperties(config, (config) => {
-    // Remove existing kotlinVersion if present
+function withKotlinVersion(config) {
+  // 1. Inject kotlinVersion into gradle.properties
+  config = withGradleProperties(config, (config) => {
     config.modResults = config.modResults.filter(
       (item) => !(item.type === 'property' && item.key === 'kotlinVersion')
     );
-    // Add kotlinVersion 2.0.0
     config.modResults.push({
       type: 'property',
       key: 'kotlinVersion',
@@ -14,4 +13,19 @@ module.exports = function withKotlinVersion(config) {
     });
     return config;
   });
-};
+
+  // 2. Remove enableBundleCompression from app/build.gradle (deprecated in RN 0.74+)
+  config = withAppBuildGradle(config, (config) => {
+    if (config.modResults.contents) {
+      config.modResults.contents = config.modResults.contents.replace(
+        /\s*enableBundleCompression\s*=\s*.*\n?/g,
+        '\n'
+      );
+    }
+    return config;
+  });
+
+  return config;
+}
+
+module.exports = withKotlinVersion;
